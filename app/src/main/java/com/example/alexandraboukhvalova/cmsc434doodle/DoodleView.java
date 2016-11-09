23c51bb9819f698f85b8e104e5b6f6b15b0783b9
Alexandra Boukhvalova <alexandraboukhvalova@Alexandras-MacBook-Air.local>
2016-11-08 23:01:45 -0500

    uploading final project with full funcitonality

--- app/src/main/java/com/example/alexandraboukhvalova/cmsc434doodle/DoodleView.java
+++ app/src/main/java/com/example/alexandraboukhvalova/cmsc434doodle/DoodleView.java
@@ -3,11 +3,19 @@ package com.example.alexandraboukhvalova.cmsc434doodle;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
+import android.graphics.Bitmap;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.util.TypedValue;
+
+import java.util.ArrayList;
 
 /**
  * Created by alexandraboukhvalova on 11/3/16.
@@ -16,7 +24,15 @@ import android.view.View;
 public class DoodleView extends View{
 
     private Paint _paintDoodle = new Paint();
+    private Paint _canvasPaint;
     private Path _path = new Path();
+    private Bitmap _bitmap;
+    private Canvas _canvas;
+    private int _paintColor = 0xFF660000;
+    private float _brushSize, _lastBrushSize;
+    private boolean _erase = false;
+    private int _paintAlpha = 255;
+    private ArrayList<Path> _paths = new ArrayList<Path>();
 
     public DoodleView(Context context) {
         super(context);
@@ -34,17 +50,61 @@ public class DoodleView extends View{
     }
 
     private void init(AttributeSet attrs, int defStyle) {
-        _paintDoodle.setColor(Color.RED);
+        _brushSize = getResources().getInteger(R.integer.medium_size);
+        _lastBrushSize = _brushSize;
+        _paintDoodle.setColor(Color.BLACK);
         _paintDoodle.setAntiAlias(true);
         _paintDoodle.setStyle(Paint.Style.STROKE);
+        _paintDoodle.setStrokeWidth(_brushSize);
+        _canvasPaint = new Paint(Paint.DITHER_FLAG);
     }
 
     @Override
     public void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-
+        canvas.drawBitmap(_bitmap, 0, 0, _canvasPaint);
         canvas.drawPath(_path, _paintDoodle);
+        //reset in case greyscale was just used
+        _canvasPaint.reset();
 
+    }
+
+    public int getPaintAlpha(){
+        return Math.round((float)_paintAlpha/255*100);
+    }
+
+    public void setPaintAlpha(int newAlpha){
+        _paintAlpha=Math.round((float)newAlpha/100*255);
+        _paintDoodle.setColor(_paintColor);
+        _paintDoodle.setAlpha(_paintAlpha);
+    }
+
+    public void clearAll(){
+        _bitmap.eraseColor(Color.TRANSPARENT);
+        _canvas.drawBitmap(_bitmap, 0, 0, _canvasPaint);
+        invalidate();
+    }
+
+    public void setBrushSize(float newSize) {
+        //update size
+        float pixelAmount = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                newSize, getResources().getDisplayMetrics());
+        _brushSize=pixelAmount;
+        _paintDoodle.setStrokeWidth(_brushSize);
+    }
+
+    public void setLastBrushSize(float lastSize){
+        _lastBrushSize=lastSize;
+    }
+    public float getLastBrushSize(){
+        return _lastBrushSize;
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        //view given size
+        super.onSizeChanged(w, h, oldw, oldh);
+        _bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        _canvas = new Canvas(_bitmap);
     }
 
     @Override
@@ -58,13 +118,32 @@ public class DoodleView extends View{
                 break;
             case MotionEvent.ACTION_MOVE:
                 _path.lineTo(touchX,touchY);
+                //draw path here
                 break;
             case MotionEvent.ACTION_UP:
+                _canvas.drawPath(_path, _paintDoodle);
+                _path.reset();
                 break;
         }
 
         invalidate();
         return true;
-        //return super.onTouchEvent(motionEvent);
+    }
+
+    public void setColor(String newColor){
+        //set color
+        invalidate();
+        _paintColor = Color.parseColor(newColor);
+        _paintDoodle.setColor(_paintColor);
+    }
+
+    public void toGrayscale()
+    {
+        ColorMatrix cm = new ColorMatrix();
+        cm.setSaturation(0);
+        ColorMatrixColorFilter f = new ColorMatrixColorFilter(cm);
+        _canvasPaint.setColorFilter(f);
+        _canvas.drawBitmap(_bitmap, 0, 0, _canvasPaint);
+        invalidate();
     }
 }
--- app/src/main/java/com/example/alexandraboukhvalova/cmsc434doodle/MainActivity.java
+++ app/src/main/java/com/example/alexandraboukhvalova/cmsc434doodle/MainActivity.java
@@ -1,13 +1,199 @@
 package com.example.alexandraboukhvalova.cmsc434doodle;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.app.Dialog;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends Activity implements OnClickListener {
+
+    private DoodleView doodleView;
+    private ImageButton _currentTool, _drawBtn, _deleteBtn, _opacityBtn, _greyscaleButton;
+    private float _smallBrush, _mediumBrush, _largeBrush;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        doodleView = (DoodleView)findViewById(R.id.drawing) ;
+        LinearLayout paintLayout = (LinearLayout)findViewById(R.id.paint_colors);
+        _currentTool = (ImageButton)paintLayout.getChildAt(0);
+        _currentTool.setImageResource(R.drawable.paint_pressed);
+
+        _smallBrush = getResources().getInteger(R.integer.small_size);
+        _mediumBrush = getResources().getInteger(R.integer.medium_size);
+        _largeBrush = getResources().getInteger(R.integer.large_size);
+
+        _drawBtn = (ImageButton)findViewById(R.id.draw_btn);
+        _drawBtn.setOnClickListener(this);
+
+        doodleView.setBrushSize(_smallBrush);
+
+        _deleteBtn = (ImageButton)findViewById(R.id.delete_btn);
+        _deleteBtn.setOnClickListener(this);
+
+        _opacityBtn = (ImageButton)findViewById(R.id.opacity_btn);
+        _opacityBtn.setOnClickListener(this);
+
+        _greyscaleButton = (ImageButton)findViewById(R.id.greyscale_btn);
+        _greyscaleButton.setOnClickListener(this);
+
+
+    }
+
+    public void paintClicked(View view){
+        doodleView.setBrushSize(doodleView.getLastBrushSize());
+        //use chosen color
+        if(view!= _currentTool){
+            //update color
+            ImageButton imgView = (ImageButton)view;
+            String color = view.getTag().toString();
+
+            doodleView.setColor(color);
+
+            imgView.setImageResource(R.drawable.paint_pressed);
+            _currentTool.setImageResource(R.drawable.paint);
+            _currentTool =(ImageButton)view;
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        //respond to clicks
+        if(view.getId()==R.id.draw_btn){
+
+            final Dialog seekDialog = new Dialog(this);
+            seekDialog.setTitle("Opacity level:");
+            seekDialog.setContentView(R.layout.brushchooser_seek);
+
+            final TextView seekText = (TextView)seekDialog.findViewById(R.id.brushchooser_txt);
+            final SeekBar seekBrushsize = (SeekBar)seekDialog.findViewById(R.id.brushchooser_seek);
+            seekBrushsize.setMax(30);
+
+            int currLevel = (int)doodleView.getLastBrushSize();
+            seekText.setText(currLevel+"dp");
+            seekBrushsize.setProgress(currLevel);
+
+            seekBrushsize.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    seekText.setText(Integer.toString(progress)+"dp");
+                }
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {}
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {}
+            });
+
+            Button brushsizeBtn = (Button)seekDialog.findViewById(R.id.brushchooser_ok);
+            brushsizeBtn.setOnClickListener(new OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    doodleView.setBrushSize(seekBrushsize.getProgress());
+                    doodleView.setLastBrushSize(seekBrushsize.getProgress());
+                    //doodleView.setPaintAlpha(seekOpacity.getProgress());
+                    seekDialog.dismiss();
+                }
+            });
+
+            seekDialog.show();
+
+        } else if(view.getId()==R.id.delete_btn) {
+            //switch to erase - choose size
+            final Dialog deleteDialog = new Dialog(this);
+            deleteDialog.setTitle("Eraser:");
+            deleteDialog.setContentView(R.layout.confirmation);
+
+            Button confirmationOkBtn = (Button)deleteDialog.findViewById(R.id.confirmation_ok);
+            confirmationOkBtn.setOnClickListener(new OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    doodleView.clearAll();
+                    deleteDialog.dismiss();
+                }
+            });
+
+            Button confirmationCancelBtn = (Button)deleteDialog.findViewById(R.id.confirmation_cancel);
+            confirmationCancelBtn.setOnClickListener(new OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    deleteDialog.dismiss();
+                }
+            });
+
+
+            deleteDialog.show();
+        } else if (view.getId()==R.id.opacity_btn) {
+            final Dialog seekDialog = new Dialog(this);
+            seekDialog.setTitle("Opacity level:");
+            seekDialog.setContentView(R.layout.opacity_chooser);
+
+            final TextView seekText = (TextView)seekDialog.findViewById(R.id.opacity_txt);
+            final SeekBar seekOpacity = (SeekBar)seekDialog.findViewById(R.id.opacity_seek);
+            seekOpacity.setMax(100);
+
+            int currLevel = doodleView.getPaintAlpha();
+            seekText.setText(currLevel+"%");
+            seekOpacity.setProgress(currLevel);
+
+            seekOpacity.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    seekText.setText(Integer.toString(progress)+"%");
+                }
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {}
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {}
+            });
+
+            Button opacityBtn = (Button)seekDialog.findViewById(R.id.opacity_ok);
+            opacityBtn.setOnClickListener(new OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    doodleView.setPaintAlpha(seekOpacity.getProgress());
+                    seekDialog.dismiss();
+                }
+            });
+
+            seekDialog.show();
+
+        } else if (view.getId()==R.id.greyscale_btn) {
+
+            final Dialog deleteDialog = new Dialog(this);
+            deleteDialog.setTitle("Greyscale:");
+            deleteDialog.setContentView(R.layout.greyscale_confirm);
+
+            Button confirmationOkBtn = (Button)deleteDialog.findViewById(R.id.confirmation_ok);
+            confirmationOkBtn.setOnClickListener(new OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    doodleView.toGrayscale();
+                    deleteDialog.dismiss();
+                }
+            });
+
+            Button confirmationCancelBtn = (Button)deleteDialog.findViewById(R.id.confirmation_cancel);
+            confirmationCancelBtn.setOnClickListener(new OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    deleteDialog.dismiss();
+                }
+            });
+
+
+            deleteDialog.show();
+        }
     }
 }
